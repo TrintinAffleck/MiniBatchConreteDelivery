61bfd869b0a88f73c0f72effc782cf828a491eb5
Trintin Affleck <t.affleck101@hotmail.com>
2024-04-27 20:40:53 -0600

    File for utility functions | DisableMultiSelect

--- Class1.cs
+++ Class1.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Windows.Forms;
 namespace MiniBatchConreteDelivery
 {
-	public class Utils : TransactionSystemForm
-	{
+	public partial class TransactionSystemForm : Form
+	{ 
 		public void DisableMultiSelect()
 		{
-			
+			ProductsDataGrid.MultiSelect = false;
+			InvoiceHistoryDataGrid.MultiSelect = false;
+			CustomerListDataGrid.MultiSelect = false;
 		}
 	}
 }
